56fc05569eacf338a38f39fda30fa5bc6f6b517d
Arby <103920908+The-Arbiter@users.noreply.github.com>
2022-09-08 16:29:07 +0400

    Changed ChainID to 5271383
    Credit to @mrtestyboy781 for this idea.

--- params/config.go
+++ params/config.go
@@ -57,7 +57,7 @@ var CheckpointOracles = map[common.Hash]*CheckpointOracleConfig{
 var (
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainID:             big.NewInt(1), //10001
+		ChainID:             big.NewInt(5271383), // 'PoW' in ascii is 50 6F 57 -> 5271383 in decimal
 		HomesteadBlock:      big.NewInt(1_150_000),
 		DAOForkBlock:        big.NewInt(1_920_000),
 		DAOForkSupport:      true,
